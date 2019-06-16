5cbafb840f175a998196d1d5739ad902ed707451
scarfacedeb <scarfacedeb@gmail.com>
2019-06-16 18:20:14 +0300

    Add required fields for publishing

--- mix.exs
+++ mix.exs
@@ -7,7 +7,9 @@ defmodule ExSwift.MixProject do
       version: "0.2.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: "An elixir client for OpenStack Swift API",
+      package: package()
     ]
   end
 
@@ -26,6 +28,14 @@ defmodule ExSwift.MixProject do
       {:jason, "~> 1.1"},
       {:typed_struct, "~> 0.1"},
       {:dialyxir, "~> 1.0.0-rc", only: [:dev, :test], runtime: false},
+    ]
+  end
+
+  defp package do
+    [
+      licenses: ["MIT"],
+      maintainers: ["Andrew Volozhanin"],
+      links: %{"Github" => "https://github.com/scarfacedeb/ex_swift"}
     ]
   end
 end
